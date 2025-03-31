eb92b68eb9d3166b6917e0f12251fe34cb4bb00b
WHF-Post <1productionkat@gmail.com>
2025-03-31 18:07:09 -0400

    Update LogoParticles.js

--- components/LogoParticles.js
+++ components/LogoParticles.js
@@ -9,29 +9,30 @@ export default function LogoParticles() {
   return (
     <div className="relative w-full h-screen flex flex-col items-center justify-center bg-black text-white">
       {/* Particle Background */}
-      <div className="absolute top-0 left-0 w-full h-full z-0">
-       <Particles
-  id="tsparticles"
-  options={{
-    fullScreen: { enable: false },
-    background: { color: '#000000' },
-    fpsLimit: 60,
-    particles: {
-      number: { value: 80 },
-      color: { value: '#00BFFF' },
-      links: {
-        enable: true,
-        color: '#00BFFF',
-        distance: 150,
-        opacity: 0.4,
-        width: 1,
+<div className="absolute top-0 left-0 w-full h-full z-0">
+  <Particles
+    id="tsparticles"
+    options={{
+      fullScreen: { enable: false },
+      background: { color: '#000000' },
+      fpsLimit: 60,
+      particles: {
+        number: { value: 80 },
+        color: { value: '#00BFFF' },
+        links: {
+          enable: true,
+          color: '#00BFFF',
+          distance: 150,
+          opacity: 0.4,
+          width: 1,
+        },
+        move: { enable: true, speed: 1 },
+        size: { value: 2 },
+        opacity: { value: 0.5 },
       },
-      move: { enable: true, speed: 1 },
-      size: { value: 2 },
-      opacity: { value: 0.5 },
-    },
-  }}
-/>
+    }}
+  />
+</div>
       {/* Foreground Content */}
       <div className="relative z-10 flex flex-col items-center text-center">
         <Image
