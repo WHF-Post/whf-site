628f2e1ce7d4d5f75f1fc07c4030e27b1645e351
WHF-Post <1productionkat@gmail.com>
2025-03-31 21:25:13 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,6 @@ export default function HomePage() {
   return (
     <div style={{ minHeight: '100vh', backgroundColor: 'black', color: 'white' }}>
       <LogoParticles />
-      <noscript>
-        <h1>💥 WHF fallback is live</h1>
-      </noscript>
     </div>
   );
 }
