f62d152bc8831986a0c224faf1e17cc951125860
WHF-Post <1productionkat@gmail.com>
2025-03-31 20:22:37 -0400

    Update LogoParticles.js

--- components/LogoParticles.js
+++ components/LogoParticles.js
@@ -5,27 +5,53 @@ import Particles from '@tsparticles/react';
 
 export default function LogoParticles() {
   return (
-    <div className="w-full h-screen bg-black text-white">
+    <div className="relative w-full h-screen bg-black">
       <Particles
         id="tsparticles"
         options={{
           fullScreen: { enable: false },
-          background: { color: '#000000' },
+          background: {
+            color: '#000000',
+          },
           fpsLimit: 60,
           particles: {
-            number: { value: 80 },
-            color: { value: '#00BFFF' },
+            number: {
+              value: 80,
+              density: {
+                enable: true,
+                area: 800,
+              },
+            },
+            color: {
+              value: '#ffffff',
+            },
+            shape: {
+              type: 'circle',
+            },
+            opacity: {
+              value: 0.5,
+            },
+            size: {
+              value: 2,
+              random: true,
+            },
             links: {
               enable: true,
-              color: '#00BFFF',
               distance: 150,
+              color: '#ffffff',
               opacity: 0.4,
               width: 1,
             },
-            move: { enable: true, speed: 1 },
-            size: { value: 2 },
-            opacity: { value: 0.5 },
+            move: {
+              enable: true,
+              speed: 1,
+              direction: 'none',
+              outModes: {
+                default: 'bounce',
+              },
+            },
           },
+          detectRetina: true,
         }}
       />
     </div>
