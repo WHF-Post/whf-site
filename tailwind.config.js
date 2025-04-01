946ac12c6c28374b7d1bdf568fe27c33964ff706
WHF-Post <1productionkat@gmail.com>
2025-04-01 15:50:16 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,3 +6,19 @@ module.exports = {
   },
   plugins: [],
 }
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      animation: {
+        typewriter: 'typewriter 2s steps(20) forwards',
+      },
+      keyframes: {
+        typewriter: {
+          'from': { width: '0' },
+          'to': { width: '100%' },
+        },
+      },
+    },
+  },
+};
