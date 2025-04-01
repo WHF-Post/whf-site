3a61f0250f55914ea0749aae18a30c3858f85d5a
WHF-Post <1productionkat@gmail.com>
2025-03-31 21:41:07 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
-import LogoParticles from '../components/LogoParticles';
+import dynamic from 'next/dynamic';
+
+const LogoParticles = dynamic(() => import('../components/LogoParticles'), {
+  ssr: false,
+});
 
 export default function HomePage() {
   return (
-    <div style={{ height: '100vh', backgroundColor: 'black' }}>
+    <div style={{ minHeight: '100vh', backgroundColor: 'black', color: 'white' }}>
       <LogoParticles />
     </div>
   );
